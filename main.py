3ee9993c73877a27a977cb9a35ceda73e9aab5af
some-du6e <103129948+some-du6e@users.noreply.github.com>
2022-12-01 10:18:16 -0600

    a

---
+++ main.py
@@ -0,0 +1,35 @@
+#
+def format(url):
+    url = url.replace("https://", "")
+
+    print("URL: " + url)
+    return url
+
+def formatfinal(url):
+    url = url.replace("%2f", "/")
+    url = "https://" + url
+
+    print("URL2: " + url)
+    return url
+
+def listToString(s):
+    str1 = ""
+    for ele in s:
+        str1 += ele
+    return str1
+
+def main():
+    linklist = [] # lmk if there is a better way to do this
+    link = input("Enter link: ")
+    link = format(link)
+    for i in range(len(link)):
+        hexstring = "%" + link[i].encode("utf-8").hex()
+        linklist += hexstring
+
+    link = listToString(linklist)
+    link = formatfinal(link)
+    print(link)
+if "__main__" == __name__:
+    main()
+
+
